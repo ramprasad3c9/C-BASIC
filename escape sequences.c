a120edb8a99233560b0d4d93daa018b06df2d84a
ramprasad3c9 <87995887+ramprasad3c9@users.noreply.github.com>
2021-10-28 12:24:30 +0530

    Add files via upload

---
+++ escape sequences.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    printf("hello\rall\n");
+	printf("how\rare\ryou\n");
+	printf("\\ this is the backslash symbol\n");
+	printf("rich dad poor dad is written by \"robets kiyosaki\"");
+}
