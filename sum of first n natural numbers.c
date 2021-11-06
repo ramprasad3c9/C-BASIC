f2365df10050509f84f10b64a695ef873dd49101
ramprasad3c9 <87995887+ramprasad3c9@users.noreply.github.com>
2021-11-06 12:24:01 +0530

    Add files via upload

---
+++ sum of first n natural numbers.c
@@ -0,0 +1,9 @@
+//write a C program to calculate the sum of first n natural numbers when n is given
+#include<stdio.h>
+void main()
+{
+	int n,formulae;
+	n=10;
+	formulae=n*(n+1)/2;
+	printf("sum of first %d natural numbers is %d",n,formulae);
+}
